9a4bb86a2b5a6301b19c5ae0d2e56fafe18d6574
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2022-11-23 08:55:51 +0530

    Create animeremux.py

---
+++ animeremux.py
@@ -0,0 +1,21 @@
+import time
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.animeremux.xyz/2022/07/my-dress-up-darling-2022-anime-series.html?m=1"
+
+def remux(url):
+    client = requests.session()
+    r = client.get(url).text
+    soup = BeautifulSoup (r, "html.parser")
+    for a in soup.find_all("a"):
+             c = a.get("href") 
+             if "appdrive.in" in c:
+                      x = c.split("url=")[-1]
+                      t = client.get(x).text
+                      soupt = BeautifulSoup(t, "html.parser")
+                      title = soupt.title
+                      gd_txt = f"{(title.text).replace('GDToT | ' , '')}\n{x}\n\n"
+                      print(gd_txt) 
+
+print(remux(url))
